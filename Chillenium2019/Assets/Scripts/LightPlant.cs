d48c7000be7ebbca7235d9ea57cf404c2c6eced6
JustAlejandro <32211773+JustAlejandro@users.noreply.github.com>
2019-10-05 15:33:10 -0500

    Added in MainSpotlight
    Added MainSpotlight and all its events

---
+++ Chillenium2019/Assets/Scripts/LightPlant.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightPlant : MonoBehaviour {
+    [Range(1, 2)]
+    public int spotID;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
