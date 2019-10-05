005daaecde47436c61756cb5446489801e2107de
Marco Ravelo <mravelo5874@gmail.com>
2019-10-04 20:10:10 -0500

    Added simple player controls

---
+++ Chillenium2019/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+   public float playerSpeed = 10; //speed player moves
+   private Rigidbody2D rb;
+
+   private void Awake()
+   {
+       rb = GetComponent<Rigidbody2D>();
+   }
+   
+   private void Update () 
+   {
+   
+       Move(); // Player Movement 
+   }
+   
+   void Move()
+   {
+       float horizontal = Input.GetAxis("Horizontal");
+       float vertical = Input.GetAxis("Vertical");
+       rb.velocity = new Vector2(horizontal * playerSpeed, vertical * playerSpeed);
+   }
+}
