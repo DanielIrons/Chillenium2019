ea3e9b7a428bc1380a8daa4a0f8a022e3e5f5d53
JustAlejandro <32211773+JustAlejandro@users.noreply.github.com>
2019-10-05 20:56:26 -0500

    Fixed the AddToList
    AddToList wasn't static, so made it that so it was easier to access.

--- Chillenium2019/Assets/Scripts/AddToList.cs
+++ Chillenium2019/Assets/Scripts/AddToList.cs
@@ -8,6 +8,7 @@ public class AddToList : MonoBehaviour
 {
     public GameObject template;
     public GameObject content;
+    private static bool getNext;
 
     public void add_Click() {
         var copy = Instantiate(template);
@@ -16,23 +17,30 @@ public class AddToList : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        getNext = false;
+    }
+
+    void Update() {
+        if (getNext) {
+            if (Event_Script.moreJobs()) {
+                List<Event_Brief> toAdd = Event_Script.jobsBrief();
+                foreach (Transform child in content.transform) {
+                    Destroy(child.gameObject);
+                }
+                foreach (Event_Brief ev in toAdd) {
+                    UnityEngine.Debug.Log(ev.getType());
+                    GameObject cur = Instantiate(template);
+                    cur.transform.parent = content.transform;
+                    cur.GetComponentInChildren<TMPro.TextMeshProUGUI>().text = Enum.GetNames(typeof(Job))[ev.getType()];
+                }
+            }
+            getNext = !getNext;
+        }
     }
 
     // Update is called once per frame
-    void showNew()
+    static void showNew()
     {
-        if (Event_Script.moreJobs()) {
-            List<Event_Brief> toAdd = Event_Script.jobsBrief();
-            foreach (Transform child in content.transform) {
-                Destroy(child.gameObject);
-            }
-            foreach (Event_Brief ev in toAdd) {
-                UnityEngine.Debug.Log(ev.getType());
-                GameObject cur = Instantiate(template);
-                cur.transform.parent = content.transform;
-                cur.GetComponentInChildren<TMPro.TextMeshProUGUI>().text = Enum.GetNames(typeof(Job))[ev.getType()];
-            }
-        }
+        getNext = true;
     }
 }
