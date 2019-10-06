1c9824ca7ef76e5b654d48792a3c93403d99c517
Marco Ravelo <mravelo5874@gmail.com>
2019-10-06 16:29:52 -0500

    added UI text

---
+++ Chillenium2019/Assets/Scripts/TextManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TextManager : MonoBehaviour
+{
+    public TextMeshProUGUI jobsDoneText;
+    public TextMeshProUGUI jobsQueueText;
+
+    void Update()
+    {
+        jobsDoneText.text = "Jobs Done: " + Event_Script.jobsDone.ToString();
+        jobsQueueText.text = "Jobs in Queue: " + Event_Script.jobsLeft.ToString();
+    }
+}
