eff83908613cf47972a0050843544cccca4166e5
Marco Ravelo <mravelo5874@gmail.com>
2019-10-06 14:31:16 -0500

    098765432

---
+++ Chillenium2019/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    public AudioSource ThemeSong;
+    public AudioSource AndrewsSong;
+
+    public void PlayTheme() {
+        StopAllSongs();
+        ThemeSong.Play();
+    }
+
+    public void PlayAndrews() {
+        StopAllSongs();
+        AndrewsSong.Play();
+    }
+
+    private void StopAllSongs() {
+        ThemeSong.Stop();
+        AndrewsSong.Stop();
+    }
+}
