e27860b268fec22c10c63ac192e10f9754f3de61
Alex Gonzales <agonzales5071@gmail.com>
2019-10-06 15:14:12 -0500

    Finished Scene Select

--- Chillenium2019/Assets/Scripts/LoadFromSelect2.cs
+++ Chillenium2019/Assets/Scripts/LoadFromSelect2.cs
@@ -7,11 +7,9 @@ using UnityEngine.SceneManagement;
 public class LoadFromSelect2 : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        GameObject.Find("GameManager").GetComponent<GameManager>().currentSong = 2;
-        SceneManager.LoadScene("GameScene");
-    }
+    
+
+   
 
     // Update is called once per frame
     void Update()
---
+++ Chillenium2019/Assets/Scripts/LoadMusicManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class LoadMusicManager : MonoBehaviour
+{
+    public void loadSong(int num)
+    {
+        GameObject.Find("GameManager").GetComponent<GameManager>().currentSong = num;
+        SceneManager.LoadScene("GameScene");
+    }
+}
